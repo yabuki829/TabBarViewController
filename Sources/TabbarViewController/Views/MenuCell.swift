891f6a7951874334917d6f6e4cd9bb8d1ed373ca
yabuki829 <syodai829@icloud.com>
2023-01-16 16:54:22 +0900

    一部ダークモード対応

--- Sources/TabbarViewController/Views/MenuCell.swift
+++ Sources/TabbarViewController/Views/MenuCell.swift
@@ -236,7 +236,7 @@ class TabCell:UICollectionViewCell{
         self.isScrollable = isScrollable
         
         if isScrollable {
-            backgroundColor = .white
+            backgroundColor = .systemBackground
         }
        
     }
